85b9ed389babd13ff726882f16ff1776114f3916
leodavinci360 <aremu@susqu.edu>
2015-04-02 23:50:31 -0400

    Final project. im defining all the buttons and trying to make the first one work with the mp3 file in the "sounds" folder

---
+++ app/src/main/java/com/example/leodavinci360/recorder/RecordActivity.java
@@ -0,0 +1,186 @@
+package com.example.leodavinci360.recorder;
+
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class RecordActivity extends ActionBarActivity {
+
+    private Button button1, button2, button3, button4, button5, button6, button7;
+    private MediaPlayer play;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                play = MediaPlayer.create(RecordActivity.this, R.sounds.clap);
+                play.start();
+                play.setOnCompletionListener(new OnCompletionListener() {
+
+                    @Override
+                    public void onCompletion(MediaPlayer play) {
+                        play.release();
+
+                    }
+                });
+
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                play = MediaPlayer.create(RecordActivity.this, R.sounds.);
+                play.start();
+                play.setOnCompletionListener(new OnCompletionListener() {
+
+                    @Override
+                    public void onCompletion(MediaPlayer play) {
+                        play.release();
+
+                    }
+                });
+
+            }
+        });
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                play = MediaPlayer.create(RecordActivity.this, R.sounds.);
+                play.start();
+                play.setOnCompletionListener(new OnCompletionListener() {
+
+                    @Override
+                    public void onCompletion(MediaPlayer play) {
+                        play.release();
+
+                    }
+                });
+
+            }
+        });
+
+        button4 = (Button) findViewById(R.id.button3);
+        button4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                play = MediaPlayer.create(RecordActivity.this, R.sounds.);
+                play.start();
+                play.setOnCompletionListener(new OnCompletionListener() {
+
+                    @Override
+                    public void onCompletion(MediaPlayer play) {
+                        play.release();
+
+                    }
+                });
+
+            }
+        });
+
+        button5 = (Button) findViewById(R.id.button3);
+        button5.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                play = MediaPlayer.create(RecordActivity.this, R.sounds.);
+                play.start();
+                play.setOnCompletionListener(new OnCompletionListener() {
+
+                    @Override
+                    public void onCompletion(MediaPlayer play) {
+                        play.release();
+
+                    }
+                });
+
+            }
+        });
+
+        button6 = (Button) findViewById(R.id.button4);
+        button6.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                play = MediaPlayer.create(RecordActivity.this, R.sounds.);
+                play.start();
+                play.setOnCompletionListener(new OnCompletionListener() {
+
+                    @Override
+                    public void onCompletion(MediaPlayer play) {
+                        play.release();
+
+                    }
+                });
+
+            }
+        });
+
+        button7 = (Button) findViewById(R.id.button5);
+        button7.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                play = MediaPlayer.create(RecordActivity.this, R.sounds.);
+                play.start();
+                play.setOnCompletionListener(new OnCompletionListener() {
+
+                    @Override
+                    public void onCompletion(MediaPlayer play) {
+                        play.release();
+
+                    }
+                });
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_record, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
